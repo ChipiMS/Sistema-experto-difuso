e6be555326dca9702b4d2302108997478d296465
Matías Mangiantini <matiasm15@hotmail.com>
2019-03-14 13:44:41 -0600

    Fix condicion de cierre del conjunto

--- src/sistemaexpertodifuso/Conjunto.java
+++ src/sistemaexpertodifuso/Conjunto.java
@@ -23,7 +23,7 @@ class Conjunto{
     		Punto primer = puntosCriticos[i];
     		Punto segundo = puntosCriticos[i + 1];
     		
-			if (!primer.esVacio() && !segundo.esVacio() && primer.x <= x && x <segundo.x) {
+			if (!primer.esVacio() && !segundo.esVacio() && primer.x <= x && x <= segundo.x) {
 			
                             return evaluarRecta(primer, segundo, x);
                                 
